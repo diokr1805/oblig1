c125f2e3b39ac0ce4511087db2cf0ef54913cdc0
diokr1805 <diokr1805@oslomet.no>
2024-02-11 22:28:48 +0100

    Validation for input fields completed

--- oblig1/src/main/resources/static/main.js
+++ oblig1/src/main/resources/static/main.js
@@ -1,8 +1,27 @@
-let billetter = [];
 
 function validering(){
-    let velgFilm = document.getElementById("velgFilmer").value;
-    if(velgFilm == ""){
+    const navnValidering = /^[a-zA-Z]/;
+    const telefonnnrValidering = /^[1-9]/
+    const epostValidering = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
 
+    let valgtFilm = document.getElementById("velgFilm").value;
+    let mengdeBilletter = document.getElementById("antallBilletter").value;
+    let fornavn = document.getElementById("oppgiFornavn").value;
+    let etternavn = document.getElementById("oppgiEtternavn").value;
+    let telefonnr = document.getElementById("oppgiTelefonnr").value;
+    let epost = document.getElementById("oppgiEpost").value;
+
+    if(valgtFilm == ""){
+        alert("Vennligst velg en film!")
+    }else if(mengdeBilletter == "" || mengdeBilletter == "0"){
+        alert("Vennligst oppgi gyldig mengde billetter!")
+    }else if(!navnValidering.test(fornavn) || fornavn == ""){
+        alert("Vennligst oppgi fornavn!")
+    }else if(!navnValidering.test(etternavn) || etternavn == ""){
+        alert("Vennligst oppgi etternavn!")
+    }else if(!telefonnnrValidering.test(telefonnr) || telefonnr == ""){
+        alert("Vennligst oppgi en gyldig telefonummer!")
+    }else if(!epostValidering.test(epost) || epost == ""){
+        alert("Vennligst oppgi en gyldig e-postaddresse")
     }
 }
